628cbe8ab188c6e5b48710f63e2f51b5cb10b124
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-13 23:09:38 +0530

    Adding record and capture btn functionality

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let video = document.querySelector("video");
+let recordBtnCont = document.querySelector(".record-btn-cont");
+let recordBtn = document.querySelector(".record-btn");
+let captureBtnCont = document.querySelector(".capture-btn-cont");
+let caotureBtn = document.querySelector(".capture-btn");
 
+let recordFlag = false;
+let recorder;
 
 
 let constraints = {
@@ -12,4 +18,25 @@ let constraints = {
 navigator.mediaDevices.getUserMedia(constraints)
 .then((stream) => {
 	video.srcObject = stream;
+
+	// API to record. What to record -> Stream
+	recorder = new MediaRecorder(stream);
+})
+
+
+recordBtnCont.addEventListener("click", (e) => {
+	if(!recorder) return;
+
+	recordFlag = !recordFlag;
+	
+	if(recordFlag){
+		//start
+		recorder.start();
+		recordBtn.classList.add("scale-record");
+	}else{
+		//stop
+		recorder.stop();
+		recordBtn.classList.remove("scale-record");
+	}
+
 })
