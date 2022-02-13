3126d42c716c5f0a23f2884f988d3e4a2caa24c8
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-14 01:38:47 +0530

    Timer Structure, Styling and Functionality

--- script.js
+++ script.js
@@ -6,62 +6,91 @@ let caotureBtn = document.querySelector(".capture-btn");
 
 let recordFlag = false;
 let recorder;
-let chunks  = []; // Media Data in Chucks
-
+let chunks = []; // Media Data in Chucks
 
 let constraints = {
-	video : true,
-	audio : true
-}
+  video: true,
+  audio: true,
+};
 
 // navigator-> global Object(Tells Browser Info)
 // mediaDevices -> helps us to connect to our hardware. (Interface)
-navigator.mediaDevices.getUserMedia(constraints)
-.then((stream) => {
-	video.srcObject = stream;
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+  video.srcObject = stream;
 
-	// API to record. What to record -> Stream
-	recorder = new MediaRecorder(stream);
+  // API to record. What to record -> Stream
+  recorder = new MediaRecorder(stream);
 
-	// Task to perfrom when we are starting recording
-	recorder.addEventListener("start", (e) => {
-		// We want to record multiple times. Therefore we have to empty
-		// our chunks array time and again.
-		chunks = [];
-	})
+  // Task to perfrom when we are starting recording
+  recorder.addEventListener("start", (e) => {
+    // We want to record multiple times. Therefore we have to empty
+    // our chunks array time and again.
+    chunks = [];
+  });
 
-	// Download the Stream
-	recorder.addEventListener("dataavailable", (e) => {
-		chunks.push(e.data);
-	})
+  // Download the Stream
+  recorder.addEventListener("dataavailable", (e) => {
+    chunks.push(e.data);
+  });
 
-	// Task to perform when we are stopping recording
-	recorder.addEventListener("stop", (e) =>{
-		// Conversion of Media chunks data to Video
-		let blob = new Blob(chunks, {type:"video/mp4"});
-		let videoURL = URL.createObjectURL(blob);
+  // Task to perform when we are stopping recording
+  recorder.addEventListener("stop", (e) => {
+    // Conversion of Media chunks data to Video
+    let blob = new Blob(chunks, { type: "video/mp4" });
+    let videoURL = URL.createObjectURL(blob);
 
-		let a  = document.createElement("a");
-		a.href = videoURL;
-		a.download = "stream.mp4";
-		a.click();
-	})
-})
-
+    let a = document.createElement("a");
+    a.href = videoURL;
+    a.download = "stream.mp4";
+    a.click();
+  });
+});
 
 recordBtnCont.addEventListener("click", (e) => {
-	if(!recorder) return;
+  if (!recorder) return;
 
-	recordFlag = !recordFlag;
-	
-	if(recordFlag){
-		//start
-		recorder.start();
-		recordBtn.classList.add("scale-record");
-	}else{
-		//stop
-		recorder.stop();
-		recordBtn.classList.remove("scale-record");
-	}
+  recordFlag = !recordFlag;
 
-})
+  if (recordFlag) {
+    //start
+    recorder.start();
+    recordBtn.classList.add("scale-record");
+  } else {
+    //stop
+    recorder.stop();
+    recordBtn.classList.remove("scale-record");
+  }
+});
+
+let timerID;
+let counter = 0; // Represents Total Seconds
+let timer = document.querySelector(".timer");
+function startTimer() {
+  timer.style.display = "block";
+  function displayTimer() {
+    let totalSeconds = counter;
+
+    let hours = Number.parseInt(totalSeconds / 3600);
+    totalSeconds = totalSeconds % 3600; // remaining value
+
+    let minutes = Number.parseInt(totalSeconds / 60);
+    totalSeconds = totalSeconds % 60; // remaining value
+
+    let seconds = totalSeconds;
+
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    timer.innerText = `${hours}:${minutes}:${seconds}`;
+
+    counter++;
+  }
+  timerID = setInterval(displayTimer, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timerID);
+  timer.innerText = "00:00:00";
+  timer.style.display = "none";
+}
